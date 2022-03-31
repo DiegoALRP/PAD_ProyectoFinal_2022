53c52140384872389edd74f2cc1e6dbd2dc3f9dc
DiegoARP <alrp.diego@gmail.com>
2022-03-31 11:23:31 +0200

    Doing test for firebase users

--- app/src/main/java/es/ucm/fdi/emtnt/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/emtnt/MainActivity.java
@@ -2,7 +2,10 @@ package es.ucm.fdi.emtnt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import es.ucm.fdi.emtnt.firebase.LogIn;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LogIn.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/es/ucm/fdi/emtnt/firebase/SignUp.java
@@ -0,0 +1,81 @@
+package es.ucm.fdi.emtnt.firebase;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import es.ucm.fdi.emtnt.R;
+
+public class SignUp extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private TextInputEditText name_input;
+    private TextInputEditText email_input;
+    private TextInputEditText password_input;
+
+    private final String TAG = "AuthEmailAndPassword";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_signup_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        name_input = findViewById(R.id.signUp_name_input_text_field);
+        email_input = findViewById(R.id.signUp_email_input_text_field);
+        password_input = findViewById(R.id.signUp_password_input_text_field);
+
+        Button login_button = findViewById(R.id.singUp_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpButtonClicked();
+            }
+        });
+    }
+
+    private void signUpButtonClicked() {
+
+        String name = String.valueOf(name_input.getText());
+        String email = String.valueOf(email_input.getText());
+        String password = String.valueOf(password_input.getText());
+
+        createNewUserEmailAndPassword(email, password);
+    }
+
+    public void createNewUserEmailAndPassword(String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(SignUp.this, "User created!", Toast.LENGTH_LONG).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUp.this, "Fail to create user", Toast.LENGTH_LONG).show();
+                        }
+
+                        SignUp.this.finish();
+                    }
+                });
+    }
+}
