1be0de57166c89f7f3bba1390003c0b7e1fa0513
Pablortu <97973045+Pablortu@users.noreply.github.com>
2022-04-06 23:33:09 +0200

    Mapa arreglado

---
+++ es/ucm/fdi/emtntr/MainActivity.java
@@ -0,0 +1,37 @@
+package es.ucm.fdi.emtntr;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button log;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        log = findViewById(R.id.Log_btn);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+        }
+
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, Nav_Activity.class);
+        startActivity(intent);
+    }
+}
