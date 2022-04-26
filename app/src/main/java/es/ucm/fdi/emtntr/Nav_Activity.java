1b1cf84e1c9092513b8ffa2004dba565772521d1
DiegoARP <alrp.diego@gmail.com>
2022-04-26 13:19:13 +0200

    Added BottomNavigationView clickListener

--- app/src/main/java/es/ucm/fdi/emtntr/Nav_Activity.java
+++ app/src/main/java/es/ucm/fdi/emtntr/Nav_Activity.java
@@ -1,15 +1,25 @@
 package es.ucm.fdi.emtntr;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import org.jetbrains.annotations.NotNull;
+
+import es.ucm.fdi.emtntr.ui.FavouriteFragment;
+import es.ucm.fdi.emtntr.ui.HomeFragment;
+import es.ucm.fdi.emtntr.ui.MapFragment;
 
 public class Nav_Activity extends AppCompatActivity {
 
@@ -18,16 +28,42 @@ public class Nav_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnItemSelectedListener(navListener);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        /*AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
         //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);*/
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener =
+            new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    selectedFragment = new FavouriteFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    selectedFragment = new MapFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, selectedFragment).commit();
+
+            return true;
+        }
+    };
 
 }
