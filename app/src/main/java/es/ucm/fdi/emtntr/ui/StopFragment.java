c4e2259a2668e8be791f42990fc7ba1c3f7376b5
DiegoARP <alrp.diego@gmail.com>
2022-04-28 22:21:42 +0200

    Adding my part to StopFragment

--- app/src/main/java/es/ucm/fdi/emtntr/ui/StopFragment.java
+++ app/src/main/java/es/ucm/fdi/emtntr/ui/StopFragment.java
@@ -1,9 +1,7 @@
 package es.ucm.fdi.emtntr.ui;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Context;
-import android.os.Build;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,7 +13,13 @@ import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Looper;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.view.LayoutInflater;
@@ -23,7 +27,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -31,14 +43,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import es.ucm.fdi.emtntr.R;
 import es.ucm.fdi.emtntr.StopArrives.ArrivalListAdapter;
 import es.ucm.fdi.emtntr.StopArrives.AsyncAdaptableLoader;
+import es.ucm.fdi.emtntr.internalStorage.WriteIE;
 import es.ucm.fdi.emtntr.model.Arrival;
 import es.ucm.fdi.emtntr.model.BusStop;
+import es.ucm.fdi.emtntr.stopSearch.BusStopLoader;
 import es.ucm.fdi.emtntr.stopSearch.BusStopResultListAdapter;
 
 /**
@@ -58,14 +70,15 @@ public class StopFragment extends Fragment {
     private RecyclerView recyclerView_arrivals;
     private ArrivalListAdapter arrivalListAdapter;
     private ArrivalsLoaderCallBacks arrivalsLoaderCallBacks;
+    private boolean ok_toAdd;
 
     private int id = 0;
-
 
     public StopFragment() {
         // Required empty public constructor
     }
 
+    // TODO: Rename and change types and number of parameters
     public static StopFragment newInstance(BusStop busStop) {
         StopFragment fragment = new StopFragment();
         Bundle args = new Bundle();
@@ -80,7 +93,6 @@ public class StopFragment extends Fragment {
         if (getArguments() != null) {
             this.busStop = (BusStop) getArguments().get(ARG_BUS_STOP);
         }
-
     }
 
     @Override
@@ -95,15 +107,26 @@ public class StopFragment extends Fragment {
         txtv1.setText(busStop.getName());
         txtv2.setText("Número: " + busStop.getId());
 
+        ok_toAdd = false;
         toggleButton.setChecked(false);
-        toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_star_empty));
+        toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.star_empty));
         toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                if(checked) toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_star));
-                else toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_star_empty));
+                if(checked) {
+
+                    if (ok_toAdd) addBusStopToFavourites();
+                    else ok_toAdd = true;
+                    toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.star_full));
+                }
+                else {
+
+                    deleteBusStopFromFavourites();
+                    toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.star_empty));
+                }
             }
         });
+        isBusStopInFavorite();
 
         recyclerView_arrivals = root.findViewById(R.id.arrivals_recyclerView);
 
@@ -189,6 +212,96 @@ public class StopFragment extends Fragment {
 
         @Override
         public void onLoaderReset(@NonNull @NotNull Loader<List<Arrival>> loader) {
+
+        }
+
+    }
+
+    //Favourites Section
+    private void addBusStopToFavourites() {
+
+        if (ok_toAdd) {
+            Resources resources = getContext().getResources();
+            String st = resources.getString(R.string.favourite_alert_favouriteAdded);
+            Toast.makeText(getContext(), st, Toast.LENGTH_SHORT).show();
+        }
+
+        LinesLoaderCallBacks linesLoaderCallBacks = new LinesLoaderCallBacks(getContext());
+
+        LoaderManager.getInstance(this).restartLoader(1, new Bundle(), linesLoaderCallBacks);
+    }
+
+    private void addLinesInfo(ArrayList<BusStop> data) {
+
+        WriteIE writeIE = new WriteIE();
+        writeIE.addAndWriteBusStopToFavourites(getActivity().getApplicationContext(), data);
+    }
+
+    private void deleteBusStopFromFavourites() {
+
+        Resources resources = getContext().getResources();
+        String st = resources.getString(R.string.favourite_alert_favouriteDeleted);
+        Toast.makeText(getContext(), st, Toast.LENGTH_SHORT).show();
+
+        WriteIE writeIE = new WriteIE();
+        writeIE.deleteBusStop(getActivity().getApplicationContext(), busStop.getId());
+    }
+
+    private void isBusStopInFavorite() {
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance("https://pad-proyectofinal-1-default-rtdb.europe-west1.firebasedatabase.app");
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getUid() != null) {
+            databaseReference.child(mAuth.getUid()).child("Favourites").child(busStop.getId())
+                    .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task<DataSnapshot> task) {
+
+                    ok_toAdd = true;
+                    if (task.isSuccessful()) {
+
+                        String st = String.valueOf(task.getResult().getValue());
+
+                        if (st.equals("null")) {
+
+                            StopFragment.this.toggleButton.setChecked(false);
+                        }
+                        else {
+                            StopFragment.this.toggleButton.setChecked(true);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public class LinesLoaderCallBacks implements LoaderManager.LoaderCallbacks<List<BusStop>> {
+
+        Context context;
+        public LinesLoaderCallBacks(Context context) {
+            this.context = context;
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public BusStopLoader onCreateLoader(int id, @Nullable @org.jetbrains.annotations.Nullable Bundle args) {
+
+            BusStopLoader busStopLoader = new BusStopLoader(context, busStop.getId(), BusStopLoader.Operation.BUS_STOP_INFO);
+
+            return busStopLoader;
+        }
+
+        @Override
+        public void onLoadFinished(@NonNull @NotNull Loader<List<BusStop>> loader, List<BusStop> data) {
+
+            addLinesInfo((ArrayList<BusStop>) data);
+        }
+
+        @Override
+        public void onLoaderReset(@NonNull @NotNull Loader<List<BusStop>> loader) {
 
         }
 
